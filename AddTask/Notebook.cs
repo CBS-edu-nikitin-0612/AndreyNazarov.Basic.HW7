23b7b4cfad39f9b20be33a99b58f69a2391d5139
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-01-30 14:57:32 +0300

    feat: Additional Task

---
+++ AddTask/Notebook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AddTask
+{
+    internal struct Notebook
+    {
+        private string model;
+        private string manufacturer;
+        private int price;
+
+        public Notebook(string model, string manufacturer, int price)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+
+        public void Print()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;           
+
+            Console.WriteLine("{0,-20}{1,-40}", "Model:", model);
+            Console.WriteLine("{0,-20}{1,-40}", "Manufacturer:", manufacturer);
+            Console.WriteLine("{0,-20}{1,-40:C0}", "Price:", price);
+        }
+    }
+}
---
+++ AddTask/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AddTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var notebook = new Notebook("SDR45F", "Lenovo", 1500);
+            notebook.Print();
+        }
+    }
+}
