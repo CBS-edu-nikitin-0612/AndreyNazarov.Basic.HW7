bf8d1c80762466805aac175cc4721f6f9013e3bc
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-01-30 15:34:56 +0300

    feat: ClassWork2

---
+++ ClassWork2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new Student[6];
+            students[0] = new Student("White", 1, 4, 3);
+            students[1] = new Student("Jones", 4, 4, 3);
+            students[2] = new Student("Smith", 3, 5, 2);
+            students[3] = new Student("Obama", 4, 4, 3);
+            students[4] = new Student("Chan", 1, 3, 2);
+            students[5] = new Student("Biden", 3, 1, 3);
+
+            Array.Sort(students);
+
+            Console.WriteLine("{0,-20}{1,10}{2,10}{3,10}{4,10}", "Second Name", "Score1", "Score2", "Score3", "Amount");
+            Console.WriteLine(new string('-', 60));
+            for (int i = 0; i < students.Length - 1; i++)
+            {
+                Console.WriteLine("{0,-20}{1,10}{2,10}{3,10}{4,10}", students[i].SecondName, students[i].Score1, students[i].Score2, students[i].Score3, students[i].GetAmountScore());
+            }
+        }
+    }
+}
---
+++ ClassWork2/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassWork2
+{
+    internal struct Student : IComparable
+    {
+        public string SecondName { get; private set; }
+        public int Score1 { get; private set; }
+        public int Score2 { get; private set; }
+        public int Score3 { get; private set; }
+
+        public Student(string secondName, int score1, int score2, int score3)
+        {
+            SecondName = secondName;
+            Score1 = score1;
+            Score2 = score2;
+            Score3 = score3;
+        }
+
+        public int CompareTo(object obj)
+        {
+
+            if (((Student)obj).GetAmountScore() < GetAmountScore())
+            {
+                return -1;
+            }
+            else
+            {
+                return ((Student)obj).GetAmountScore() == GetAmountScore() ? 0 : 1;
+            }
+        }
+
+        public int GetAmountScore()
+        {
+            return Score1 + Score2 + Score3;
+        }
+    }
+}
