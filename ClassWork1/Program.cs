fe2292418ff12252697cfc61c8c17ea2933dd43a
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-01-30 15:16:59 +0300

    feat: ClassWork1

---
+++ ClassWork1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new Student[6];
+            students[0] = new Student("White", 1);
+            students[1] = new Student("Jones", 4);
+            students[2] = new Student("Smith", 3);
+            students[3] = new Student("Obama", 7);
+            students[4] = new Student("Chan", 1);
+            students[5] = new Student("Biden", 3);
+
+            Console.WriteLine("{0,-20}{1,-10}", "Second Name", "Number");
+            Console.WriteLine(new string('-', 30));
+            for (int i = 0; i < students.Length - 1; i++)
+            {
+                Console.WriteLine("{0,-20}{1,-10}", students[i].SecondName, students[i].Number);
+            }
+        }
+    }
+}
---
+++ ClassWork1/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassWork1
+{
+    internal struct Student
+    {
+        public string SecondName { get; private set; }
+        public int Number { get; private set; }
+
+        public Student(string secondName, int number)
+        {
+            SecondName = secondName;
+            Number = number;
+        }
+    }
+}
